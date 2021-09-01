ec3e5389790803f1e5cebb2637036180ecf258a1
omarakamal <54825038+omarakamal@users.noreply.github.com>
2021-09-01 19:44:02 +0300

    Create optional Parameter.py

---
+++ optional Parameter.py
@@ -0,0 +1,10 @@
+#optional parameters
+def func(x, text='2'): #optional parameter is putting = after the variable to give it n optional parameter
+    print(x)
+    if text == '1':
+        print("text is equal to 1")
+    else:
+        print("text is not equal to 1")
+
+
+func('hello')
