79edc837d29628f6aed212aff7767ef87fbc8d40
omarakamal <54825038+omarakamal@users.noreply.github.com>
2021-09-02 19:30:48 +0300

    Create String Methods.py

---
+++ String Methods.py
@@ -0,0 +1,9 @@
+#string method
+# .strip() strips the spaces before and aftet the string
+# len() gives you the length of the characters in the string. its a function
+# .split() creates a list out of the string you give it
+#.upper makes the letters uppercase
+#lower (makes the letters lowercase)
+
+text = input("input something: ")
+print(text.split(' '))
